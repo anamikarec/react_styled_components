5efbcd78ed2f6b36293a7d27b17f9f9a27ed2aae
Anamika Gupta <65801101+anamikarec@users.noreply.github.com>
2021-11-30 18:18:41 +0530

    changes after adding Styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import "./styles.css";
+import styled from "styled-components";
 
+const Sat = styled.div`
+  border: 1px solid red;
+
+  h1 {
+    color: blue;
+  }
+  h2 {
+    color: red;
+  }
+`;
+
+const Cat = styled.a`
+  font-size: 24px;
+  text-decoration: none;
+`;
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <Sat className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </Sat>
+      <Cat href="https://www.google.com"> Go to Google</Cat>
+    </>
   );
 }
